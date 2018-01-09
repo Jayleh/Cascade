368c62c8b796efef7e57d6aae1baf395c7f48e28
Justin Le <jaylehyun@gmail.com>
2018-01-08 16:19:01 -0800

    adding files

---
+++ Cascade API.py
@@ -0,0 +1,80 @@
+from __future__ import print_function
+import os
+import time
+import httplib2
+
+from apiclient import discovery
+from googleapiclient.http import MediaFileUpload
+from oauth2client import client
+from oauth2client import tools
+from oauth2client.file import Storage
+
+try:
+    import argparse
+    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
+except ImportError:
+    flags = None
+
+# If modifying these scopes, delete your previously saved credentials
+# at ~/.credentials/drive-python-quickstart.json
+SCOPES = 'https://www.googleapis.com/auth/drive.file'
+CLIENT_SECRET_FILE = 'client_secret.json'
+APPLICATION_NAME = 'Cascade'
+
+def get_credentials():
+    """Gets valid user credentials from storage.
+
+    If nothing has been stored, or if the stored credentials are invalid,
+    the OAuth2 flow is completed to obtain the new credentials.
+
+    Returns:
+        Credentials, the obtained credential.
+    """
+    home_dir = os.path.expanduser('~')
+    credential_dir = os.path.join(home_dir, '.credentials')
+    if not os.path.exists(credential_dir):
+        os.makedirs(credential_dir)
+    credential_path = os.path.join(credential_dir,
+                                   'Cascade.json')
+
+    store = Storage(credential_path)
+    credentials = store.get()
+    if not credentials or credentials.invalid:
+        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
+        flow.user_agent = APPLICATION_NAME
+        if flags:
+            credentials = tools.run_flow(flow, store, flags)
+        else: # Needed only for compatibility with Python 2.6
+            credentials = tools.run(flow, store)
+        print('Storing credentials to ' + credential_path)
+    return credentials
+
+def main():
+    # Upload CSV file to drive.
+    credentials = get_credentials()
+    http = credentials.authorize(httplib2.Http())
+    drive_service = discovery.build('drive', 'v3', http=http)
+
+    # Rename CSV file with date and time
+    timestr = time.strftime("%Y%m%d-%H%M%S")
+    old_file = os.path.join(r"C:\Users\Justin\Desktop\Cascade CSV Export Files", "SalesEnquiryList.csv")
+    new_file = os.path.join(r"C:\Users\Justin\Desktop\Cascade CSV Export Files", "SalesEnquiryList " + timestr + ".csv")
+    os.rename(old_file, new_file)
+
+    folder_id = "1JUpm6FzLF5ebIT-uY-UfrsFtj9z6ilYG"
+    file_metadata = {
+        'name': new_file,
+        'mimeType': 'application/vnd.google-apps.spreadsheet',
+        'parents': [folder_id]
+    }
+    media = MediaFileUpload(new_file,
+                            mimetype='text/csv',
+                            resumable=True)
+    file = drive_service.files().create(body=file_metadata,
+                                    media_body=media,
+                                    fields='id').execute()
+    if file:
+        print('File ID: %s' % file.get('id'))
+
+if __name__ == '__main__':
+    main()
---
+++ Full Cascade Code.py
@@ -0,0 +1,63 @@
+from __future__ import print_function
+import unittest
+import os
+import time
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+
+import httplib2
+
+from apiclient import discovery
+from googleapiclient.http import MediaFileUpload
+from oauth2client import client
+from oauth2client import tools
+from oauth2client.file import Storage
+
+class Test(unittest.TestCase):
+
+    def setUp(self):
+        chromePath = r"D:\Chrome Downloads\webdrivers\chromedriver.exe"
+        chromeOptions = webdriver.ChromeOptions()
+        prefs = {"download.default_directory" : r"C:\Users\Justin\Desktop\Cascade CSV Export Files"}
+        chromeOptions.add_experimental_option("prefs", prefs)
+        
+        self.driver = webdriver.Chrome(executable_path=chromePath, chrome_options=chromeOptions)
+        self.driver.get("https://ap.unleashedsoftware.com/v2/Enquiry/SalesEnquiry")
+                
+    def test_Unleashed(self):
+        driver = self.driver
+        
+        # Log In into Unleashed
+        UnleashedUsername = "connor@cellese.com"
+        UnleashedPassword = "3rZup3WiqEi9"
+        emailFieldID = "username"
+        passFieldID = "password"
+        loginButtonXpath = """//input[@value='Log In']"""
+        
+        emailFieldElement = WebDriverWait(driver, 10).until(lambda driver: driver.find_element_by_id(emailFieldID))
+        passFieldElement = WebDriverWait(driver, 10).until(lambda driver: driver.find_element_by_id(passFieldID))
+        loginButtonElement = WebDriverWait(driver, 10).until(lambda driver: driver.find_element_by_xpath(loginButtonXpath))
+
+        emailFieldElement.clear()
+        emailFieldElement.send_keys(UnleashedUsername)
+        passFieldElement.clear()
+        passFieldElement.send_keys(UnleashedPassword)
+        loginButtonElement.click()
+        
+        # Export CSV file
+        exportButtonXpath = """//*[@id="ddbExport"]/dl/dd/ul/li[2]/a"""
+        
+        exportDropdownElement = WebDriverWait(driver, 10).until(lambda driver: driver.find_element_by_class_name("arrowDown"))
+        exportDropdownElement.click()
+                
+        exportButtonElement = WebDriverWait(driver, 10).until(lambda driver: driver.find_element_by_xpath(exportButtonXpath))
+        exportButtonElement.click()
+       
+        # Wait for file to download
+        time.sleep(10)
+           
+    def tearDown(self):
+        self.driver.quit()
+
+if __name__ == '__main__':
+    unittest.main()
